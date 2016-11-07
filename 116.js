a4eaf2065f0a04b90163a8ffcef793c2a8eb672f
mualado104 <mualado107@gmail.com>
2016-11-07 14:17:46 +0700

    Create 116.js

---
+++ 116.js
@@ -0,0 +1,10 @@
+function passCustom(){ 
+	simpleCart.checkoutTo = Custom;
+	simpleCart.email = &quot;mualado103@gmail.com&quot;
+	simpleCart.currency = VND;
+	simpleCart.successUrl = &quot;http://www.thanhtungsmartphone.com/&quot;
+} 
+function passGoogle(){ 
+	simpleCart.checkoutTo = Email; 		
+	simpleCart.currency = VND;
+}
